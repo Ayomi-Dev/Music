cbba1517d1d3f3535053fc577c5f5e9e6959c355
Ayomide Olakunle <ayomideolakunlea@gmail.com>
2022-12-30 01:03:21 +0100

    init
    Signed-off-by: Ayomide Olakunle <ayomideolakunlea@gmail.com>

--- song.js
+++ song.js
@@ -218,7 +218,7 @@ const showSong = () => {
                 songTitle.innerHTML = name;
                 songDownload.setAttribute('download', name)
             })
-        })
+        });
     });
 }
 showSong();
